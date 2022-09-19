284369ecbe564d63583db75ddc5a693ed587b617
Edgar CORTES <edgar.cor.hdz@gmail.com>
2022-09-19 15:17:05 +0200

    #2 Created User model
    Created User model with roles of employees:
    - Created User model in users/models.py
    - Generated migrations
    - Added User model in users/admin.py

--- crm/users/models.py
+++ crm/users/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        MANAGEMENT = ("MANAGEMENT", "Management")
+        SALE = ("SALE", "Sales")
+        SUPPORT = ("SUPPORT", "Support")
+
+    role = models.CharField(max_length=25, choices=Role.choices, verbose_name='role')
