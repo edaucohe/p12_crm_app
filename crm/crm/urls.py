210870293118756fffdcb810328df8b73671294a
Edgar CORTES <edgar.cor.hdz@gmail.com>
2022-09-28 10:45:38 +0200

    #7 Added contracts endpoint
    Added ../contracts/ endpoint:
    - Added contract_router at urls.py

--- crm/crm/urls.py
+++ crm/crm/urls.py
@@ -15,15 +15,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_nested import routers
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from users.views import SignUpViewSet, UserViewSet
 from customers.views import CustomerViewSet
+from contracts.views import ContractViewSet
 
 router = DefaultRouter()
+# /users/   ||   /users/{id}/
 router.register(r'users', UserViewSet, basename='users')
+# /customers/   ||   /customers/{id}/
 router.register(r'customers', CustomerViewSet, basename='customers')
+
+# /customers/{id}/contracts/   ||   /customers/{id}/contracts/{id}
+contract_router = routers.NestedSimpleRouter(router, r'customers', lookup='customers')
+contract_router.register(r'contracts', ContractViewSet, basename='contract')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,4 +39,5 @@ urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('signup/', SignUpViewSet.as_view(), name='signup'),
     path(r'', include(router.urls)),
+    path(r'', include(contract_router.urls)),
 ]
