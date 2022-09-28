67035ae5fbb81e97b50176ae7330f32756dab2fa
Edgar CORTES <edgar.cor.hdz@gmail.com>
2022-09-28 10:44:06 +0200

    #5 Implemented contracts list
    Implemented display contract list:
    - Added list method in contracts/views.py with authorizations
    - Created contrcts/serializers.py

---
+++ crm/contracts/serializers.py
@@ -0,0 +1,18 @@
+from django.contrib.auth import get_user_model
+from rest_framework.serializers import ModelSerializer, CharField
+
+from contracts.models import Contract
+
+
+class ContractSerializer(ModelSerializer):
+
+    class Meta:
+        model = Contract
+        fields = [
+            'id',
+            'amount',
+            'payment_due',
+            'status',
+            'customer',
+            'user',
+        ]
--- crm/contracts/views.py
+++ crm/contracts/views.py
@@ -1,3 +1,36 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from contracts.serializers import ContractSerializer
+
+from contracts.models import Contract
+from users import services
+from customers.models import Customer
+
+
+class ContractViewSet(ModelViewSet):
+    serializer_class = ContractSerializer
+    permission_classes = (IsAuthenticated,)
+    http_method_names = ['get', 'post', 'put', 'delete']
+
+    def get_queryset(self):
+        return Contract.objects.all()
+
+    def list(self, request, customers_pk=None, *args, **kwargs):
+        current_user = request.user
+        customer = Customer.objects.filter(pk=customers_pk).get()
+
+        if not customer.user == current_user:
+            return Response({'message': 'Customer is not assigned to you'}, status=status.HTTP_200_OK)
+
+        contracts = [contract for contract in Contract.objects.filter(customer=customer, user=current_user)]
+        contracts = sorted(contracts, key=lambda order_by: order_by.id)
+
+        if not contracts:
+            return Response({'message': 'Customer does not have contracts'}, status=status.HTTP_200_OK)
+
+        return Response(self.serializer_class(contracts, many=True).data, status=status.HTTP_200_OK)
