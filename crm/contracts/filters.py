3742918ad5ad208320a0f6b5bf11fa53936f497d
Edgar CORTES <edgar.cor.hdz@gmail.com>
2022-10-05 11:35:58 +0200

    #8 Implemented email filter for contracts
    Implemented filter feature for customer email in contracts list:
    - Updated contracts/filters.py

--- crm/contracts/filters.py
+++ crm/contracts/filters.py
@@ -4,13 +4,12 @@ from contracts.models import Contract
 
 
 class ContractFilterSet(filters.FilterSet):
-    customer_id = filters.CharFilter(field_name="customer_id", lookup_expr='exact')
-    # email = filters.CharFilter(field_name="email", lookup_expr='icontains')
-    # amount = filters.NumberFilter(field_name="amount", lookup_expr='icontains')
+    company_name = filters.CharFilter(field_name="customer", lookup_expr='company_name')
+    email = filters.CharFilter(field_name="customer", lookup_expr='email')
     min_amount = filters.NumberFilter(field_name="amount", lookup_expr='gte')
     max_amount = filters.NumberFilter(field_name="amount", lookup_expr='lte')
     payment_due = filters.DateFilter(field_name="payment_due", lookup_expr='icontains')
 
     class Meta:
         model = Contract
-        fields = ['customer_id', 'min_amount', 'max_amount', 'payment_due']
+        fields = ['company_name', 'email', 'min_amount', 'max_amount', 'payment_due']
